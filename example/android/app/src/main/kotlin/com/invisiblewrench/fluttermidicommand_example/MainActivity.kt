b585466eb5ed19eb26d772885540c8119180a055
bismark <info@bismark.jp>
2022-04-15 20:35:05 +0900

    Workaround to force terminate VirtualDeviceService of Android

--- example/android/app/src/main/kotlin/com/invisiblewrench/fluttermidicommand_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/invisiblewrench/fluttermidicommand_example/MainActivity.kt
@@ -1,6 +1,36 @@
 package com.invisiblewrench.fluttermidicommand_example
 
 import io.flutter.embedding.android.FlutterActivity
+import android.app.Service
+import android.content.ComponentName
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.os.IBinder
+
 
 class MainActivity: FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startService(Intent(this, DetectTerminationService::class.java))
+    }
+
+}
+
+class DetectTerminationService: Service() {
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null;
+    }
+
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        // Force terminate VirtualDeviceService
+        val context = this.applicationContext
+        val comp = ComponentName(context, "com.invisiblewrench.fluttermidicommand.VirtualDeviceService")
+        val pm = context.packageManager
+        pm.setComponentEnabledSetting(comp, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP)
+
+        stopSelf()
+    }
 }
